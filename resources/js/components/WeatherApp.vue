6ed143b320e86b3f9303bff7382ad23bb680ac1c
EmelJahaj <emeljahaj1@gmail.com>
2020-10-28 01:13:40 +0100

    first commit

---
+++ resources/js/components/WeatherApp.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="text-white mb-8 w-1/4">
+        <div class="places-input">
+            <input type="text" class="w-full">
+        </div>
+        <div class="weather-container font-sans w-128 max-w-lg overflow-hidden rounded-lg bg-gray-900 shadow-lg mt-4">
+            <div class="current-weather flex items-center justify-between px-6 py-8">
+                <div class="flex items-center"> 
+                    <div>
+                        <div class="text-6xl font-semibold">8°C</div>
+                        <div>Feels like 2°C</div>
+                    </div>
+                    <div class="mx-5">
+                        <div class="font-semibold">
+                            Cloudy
+                        </div>
+                        <div>Toronto, Canada</div>
+                    </div>
+                </div>
+                <div> icon </div>
+            </div> <!-- end current weather -->
+            <div class="future-weather text-sm bg-gray-800 px-6 py-8 overflow-hidden">
+                <div class="flex items-center">
+                    <div class="w-1/6 text-lg text-gray-200">MON</div>
+                    <div class="w-4/6 px-4 flex items-center">
+                        <div>icon</div>
+                        <div class="ml-3">Cloudy with a chance of showers</div>
+                    </div>
+                    <div class="w-1/6 text-right">
+                        <div>3°C</div>
+                        <div>-2°C</div>
+                    </div>
+                </div>
+                <div class="flex items-center mt-8">
+                    <div class="w-1/6 text-lg text-gray-200">MON</div>
+                    <div class="w-4/6 px-4 flex items-center">
+                        <div>icon</div>
+                        <div class="ml-3">Cloudy with a chance of showers</div>
+                    </div>
+                    <div class="w-1/6 text-right">
+                        <div>3°C</div>
+                        <div>-2°C</div>
+                    </div>
+                </div>
+                <div class="flex items-center mt-8">
+                    <div class="w-1/6 text-lg text-gray-200">MON</div>
+                    <div class="w-4/6 px-4 flex items-center">
+                        <div>icon</div>
+                        <div class="ml-3">Cloudy with a chance of showers</div>
+                    </div>
+                    <div class="w-1/6 text-right">
+                        <div>3°C</div>
+                        <div>-2°C</div>
+                    </div>
+                </div>
+            </div> <!-- end future weather -->
+        </div>  <!-- end weather container -->
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+          this.fetchData()
+        },
+
+        methods: {
+            
+        }
+    }
+</script>  
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather App</title>
+
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="/js/app.js" defer></script>
+</head>
+<body class="bg-blue-300">
+    <div id="app" class="flex justify-center pt-16">
+
+        <weather-app></weather-app>
+
+    </div>
+</body>
+</html>
